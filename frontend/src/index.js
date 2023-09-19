76ddb745e52856811c5788f33647edd098fc62dc
jagdish47 <jagdishkumawat81@gmail.com>
2023-09-19 16:00:29 +0530

    setup backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import "./assets/styles/bootstrap.custom.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 import {
   createBrowserRouter,
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
